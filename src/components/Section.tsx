97843affdb3d58177f04ab9c3951109143bd9746
Sami @JUNIORCO <sami.juniorco@gmail.com>
2024-07-28 01:52:11 -0400

    cmt

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,20 +1,24 @@
 import type { ComponentProps, FC, ReactNode } from "react";
 
 const Section: FC<{
-	title: string;
-	containerClassName?: ComponentProps<"div">["className"];
-	dividerClassName?: ComponentProps<"div">["className"];
-	children: ReactNode;
-}> = ({ title, containerClassName, dividerClassName, children }) => {
-	return (
-		<div className={`flex flex-col ${containerClassName}`}>
-			<p className="font-medium text-lg">{title}</p>
-			<hr
-				className={`border-t-4 border-gray-300 dark:border-gray-500 mb-2 w-20 ${dividerClassName}`}
-			/>
-			{children}
-		</div>
-	);
+  title: string;
+  subtitle?: string;
+  containerClassName?: ComponentProps<"div">["className"];
+  dividerClassName?: ComponentProps<"div">["className"];
+  children: ReactNode;
+}> = ({ title, subtitle, containerClassName, dividerClassName, children }) => {
+  return (
+    <div className={`flex flex-col ${containerClassName}`}>
+      <div className="flex justify-between">
+        <p className="font-medium text-lg">{title}</p>
+        <p>{subtitle}</p>
+      </div>
+      <hr
+        className={`border-t-4 border-gray-300 dark:border-gray-500 mb-2 w-20 ${dividerClassName}`}
+      />
+      {children}
+    </div>
+  );
 };
 
 export default Section;
