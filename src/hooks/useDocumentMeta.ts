d804774ea848cd243a1adc086bfc1bef4e6046f6
Sami @JUNIORCO <sami.juniorco@gmail.com>
2024-07-29 01:03:59 -0400

    cmt

--- src/hooks/useDocumentMeta.ts
+++ src/hooks/useDocumentMeta.ts
@@ -7,8 +7,8 @@ function useDocumentMeta(title: string, description: string) {
     let metaDescription = document.querySelector('meta[name="description"]');
 
     if (!metaDescription) {
-      metaDescription = document.createElement("meta");
-      metaDescription.name = "description";
+      metaDescription = document.createElement("meta") as HTMLMetaElement;
+      metaDescription.setAttribute("name", "description");
       document.head.appendChild(metaDescription);
     }
 
